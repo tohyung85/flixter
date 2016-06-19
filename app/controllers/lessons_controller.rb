e0b067b5270c8eeb2e7bdcc8d13f9e3231f83092
Your Name <tantohyung@gmail.com>
2016-06-19 02:47:39 +0000

    Validation of user enrollment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_before_access_lessons
   def show
     
   end
@@ -8,5 +10,11 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrollment_before_access_lessons
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      return redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in the course first!'
+    end
   end
 end
