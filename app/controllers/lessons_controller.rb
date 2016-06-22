6c6a565e2eb3e5710591934b5d880b1b13a9921b
Your Name <tantohyung@gmail.com>
2016-06-22 13:34:00 +0000

    Refactor next lesson functionality

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,15 @@ class LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_enrollment_before_access_lessons
   def show
-    
+    ### Workable code in controller to implement next lesson function
+    ### Not very good; better to include method in Lessons model so we can get the next lesson in any other controller
+ #   section = current_lesson.section
+ #   lessons = section.lessons.rank(:row_order)
+ #   if lessons.index(current_lesson) <= lessons.length
+ #     @next_lesson = lessons[lessons.index(current_lesson) + 1]
+ #   else
+ #     @next_lesson = nil
+ #   end
   end
 
   private
