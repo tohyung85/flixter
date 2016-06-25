29a8da7e1adbfcf597e1bd51c6360ba18a944fed
Your Name <tantohyung@gmail.com>
2016-06-25 06:33:30 +0000

    Added Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,12 @@ class StaticPagesController < ApplicationController
   def privacy
     
   end
+
+  def team
+    
+  end
+
+  def careers
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   get 'privacy', to: 'static_pages#privacy'
+  get 'team', to: 'static_pages#team'
+  get 'careers', to: 'static_pages#careers'
   resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
